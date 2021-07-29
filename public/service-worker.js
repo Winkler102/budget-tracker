8d6ab4a107632d3aaf3f31fe17eb3a64583dc0ed
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-07-28 21:52:09 -0500

    fixed bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,84 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
+const CACHE_NAME = 'budeget-tracker';
+const DATA_CACHE_NAME = 'data-cache';
+
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./css/styles.css",
-  "./js/idb.js",
-  "./js/index.js"
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/js/idb.js",
+  "/js/index.js",
+  "/manifest.json",
+  "/service-worker.js"
 ];
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+self.addEventListener('install', function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('You files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
     })
-  )
-})
+  );
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
+  self.skipWaiting();
+});
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
-  )
-})
+  );
 
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
+  self.clients.claim();
+});
 
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      })
-
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i]);
-          return caches.delete(keyList[i]);
+// Intercept fetch requests
+self.addEventListener('fetch', function (evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch(err => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
         }
-      }));
+      });
     })
   );
 });
